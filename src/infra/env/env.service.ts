b14ce7f20ee11eaf2cb9e0b6ad37f8fbdd396886
Jackson <jackson1712@hotmail.com>
2023-12-13 19:23:43 -0300

    feat: implements user decorator and jwt auth guard with jwt strategy

--- src/infra/env/env.service.ts
+++ src/infra/env/env.service.ts
@@ -4,7 +4,7 @@ import { Env } from './env'
 
 @Injectable()
 export class EnvService {
-  constructor(private configService: ConfigService) {}
+  constructor(private configService: ConfigService<Env, true>) {}
 
   get<T extends keyof Env>(key: T) {
     return this.configService.get(key, { infer: true })
