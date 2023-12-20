7c985c587a0abbb6da4374ec57fea97fbdd060ff
Jackson <jackson1712@hotmail.com>
2023-12-20 13:26:23 -0300

    feat: create in memory repository to users

---
+++ test/repositories/in-memory-users-repository.ts
@@ -0,0 +1,36 @@
+import { UsersRepository } from '@/domain/easy-work/application/repositories/users-repository'
+import { User } from '@/domain/easy-work/enterprise/entities/user'
+
+export class InMemoryUsersRepository implements UsersRepository {
+  public items: User[] = []
+
+  async create(user: User): Promise<void> {
+    this.items.push(user)
+  }
+
+  async findById(id: string): Promise<User | null> {
+    const user = this.items.find((item) => item.id.toString() === id)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = this.items.find((item) => item.email.toString() === email)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+
+  async save(user: User): Promise<void> {
+    const itemIndex = this.items.findIndex((item) => item.id === user.id)
+
+    this.items[itemIndex] = user
+  }
+}
