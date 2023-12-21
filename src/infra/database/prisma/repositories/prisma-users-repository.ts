f74f56884d32fe0fdf9e0c8e0e203fc4a39f25bc
Jackson <jackson1712@hotmail.com>
2023-12-21 13:37:00 -0300

    feat: add prisma repositories

---
+++ src/infra/database/prisma/repositories/prisma-users-repository.ts
@@ -0,0 +1,57 @@
+import { UsersRepository } from '@/domain/easy-work/application/repositories/users-repository'
+import { User } from '@/domain/easy-work/enterprise/entities/user'
+import { Injectable } from '@nestjs/common'
+import { PrismaUserMapper } from '../mappers/prisma-user-mapper'
+import { PrismaService } from '../prisma.service'
+
+@Injectable()
+export class PrismaUsersRepository implements UsersRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async create(user: User): Promise<void> {
+    const data = PrismaUserMapper.toPrisma(user)
+
+    await this.prisma.user.create({
+      data,
+    })
+  }
+
+  async findById(id: string): Promise<User | null> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    if (!user) {
+      return null
+    }
+
+    return PrismaUserMapper.toDomain(user)
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (!user) {
+      return null
+    }
+
+    return PrismaUserMapper.toDomain(user)
+  }
+
+  async save(user: User): Promise<void> {
+    const data = PrismaUserMapper.toPrisma(user)
+
+    await this.prisma.user.update({
+      where: {
+        id: data.id,
+      },
+      data,
+    })
+  }
+}
