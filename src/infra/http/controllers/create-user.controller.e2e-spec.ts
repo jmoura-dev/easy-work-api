2bba4310745eac34d19116199af065622bc21cba
Jackson <jackson1712@hotmail.com>
2023-12-21 18:52:49 -0300

    feat: create test e2e to creation user

---
+++ src/infra/http/controllers/create-user.controller.e2e-spec.ts
@@ -0,0 +1,40 @@
+import request from 'supertest'
+import { AppModule } from '@/infra/app.module'
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+
+describe('Create User Controller(E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+    prisma = moduleRef.get(PrismaService)
+
+    await app.init()
+  })
+
+  test('[POST] /users', async () => {
+    const response = await request(app.getHttpServer()).post('/users').send({
+      name: 'John Doe',
+      email: 'john@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(201)
+
+    const userOnDatabase = await prisma.user.findUnique({
+      where: {
+        email: 'john@example.com',
+      },
+    })
+
+    expect(userOnDatabase).toBeTruthy()
+    expect(userOnDatabase?.name).toEqual('John Doe')
+  })
+})
