61329978a1f83251c715bb86808d3a8ba01fcdfd
Jackson <jackson1712@hotmail.com>
2023-12-21 18:53:47 -0300

    feat: add new factory to create user on database

--- test/factories/make-user.ts
+++ test/factories/make-user.ts
@@ -1,6 +1,9 @@
 import { faker } from '@faker-js/faker'
 import { User, UserProps } from '@/domain/easy-work/enterprise/entities/user'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { PrismaUserMapper } from '@/infra/database/prisma/mappers/prisma-user-mapper'
 
 export function makeUser(
   override: Partial<UserProps> = {},
@@ -17,4 +20,19 @@ export function makeUser(
   )
 
   return user
+}
+
+@Injectable()
+export class UserFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaUser(data: Partial<UserProps> = {}) {
+    const user = makeUser(data)
+
+    await this.prisma.user.create({
+      data: PrismaUserMapper.toPrisma(user),
+    })
+
+    return user
+  }
 }
