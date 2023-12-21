51a1281b94cb551a8ab65f122d2a6eda0fabbf20
Jackson <jackson1712@hotmail.com>
2023-12-21 13:35:31 -0300

    feat: add file to save all controllers

---
+++ src/infra/http/controllers/authenticate-user.controller.ts
@@ -0,0 +1,52 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Post,
+  UnauthorizedException,
+} from '@nestjs/common'
+import { z } from 'zod'
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe'
+import { AuthenticateUserUseCase } from '@/domain/easy-work/application/use-cases/authenticate-user'
+import { WrongCredentialsError } from '@/domain/easy-work/application/use-cases/errors/wrong-credentials-error'
+
+const authenticateBodySchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+})
+
+const bodyValidationPipe = new ZodValidationPipe(authenticateBodySchema)
+
+type AuthenticateBodySchema = z.infer<typeof authenticateBodySchema>
+
+@Controller('/sessions')
+export class AuthenticateUserController {
+  constructor(private authenticateUserUseCase: AuthenticateUserUseCase) {}
+
+  @Post()
+  async handle(@Body(bodyValidationPipe) body: AuthenticateBodySchema) {
+    const { email, password } = body
+
+    const result = await this.authenticateUserUseCase.execute({
+      email,
+      password,
+    })
+
+    if (result.isLeft()) {
+      const error = result.value
+
+      switch (error.constructor) {
+        case WrongCredentialsError:
+          throw new UnauthorizedException(error.message)
+        default:
+          throw new BadRequestException(error.message)
+      }
+    }
+
+    const { accessToken } = result.value
+
+    return {
+      access_token: accessToken,
+    }
+  }
+}
---
+++ src/infra/http/controllers/create-user.controller.ts
@@ -0,0 +1,54 @@
+import { CreateUserUseCase } from '@/domain/easy-work/application/use-cases/create-user'
+import {
+  BadRequestException,
+  Body,
+  ConflictException,
+  Controller,
+  HttpCode,
+  Post,
+} from '@nestjs/common'
+import { z } from 'zod'
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe'
+import { EmailAlreadyExists } from '@/domain/easy-work/application/use-cases/errors/email-already-exists-error'
+
+const createUserBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string().min(6),
+  avatarId: z.string().uuid().optional(),
+  about: z.string().optional(),
+})
+
+const bodyValidationPipe = new ZodValidationPipe(createUserBodySchema)
+
+type CreateUserBodySchema = z.infer<typeof createUserBodySchema>
+
+@Controller('/users')
+export class CreateUserController {
+  constructor(private createUserUseCase: CreateUserUseCase) {}
+
+  @Post()
+  @HttpCode(201)
+  async create(@Body(bodyValidationPipe) body: CreateUserBodySchema) {
+    const { name, email, password, avatarId, about } = body
+
+    const result = await this.createUserUseCase.execute({
+      name,
+      email,
+      password,
+      avatarId,
+      about,
+    })
+
+    if (result.isLeft()) {
+      const error = result.value
+
+      switch (error.constructor) {
+        case EmailAlreadyExists:
+          throw new ConflictException(error.message)
+        default:
+          throw new BadRequestException(error.message)
+      }
+    }
+  }
+}
