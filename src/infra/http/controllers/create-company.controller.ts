47792a3f333056e5287ab8b029d8288af2587003
Jackson <jackson1712@hotmail.com>
2023-12-26 16:56:05 -0300

    feat: add new controller to create a company

---
+++ src/infra/http/controllers/create-company.controller.ts
@@ -0,0 +1,55 @@
+import { CreateCompanyUseCase } from '@/domain/easy-work/application/use-cases/create-company'
+import { Public } from '@/infra/auth/public'
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  NotAcceptableException,
+  Post,
+} from '@nestjs/common'
+import { z } from 'zod'
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe'
+import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
+
+const createBodySchema = z.object({
+  userId: z.string().uuid(),
+  cnpj: z.string(),
+  city: z.string().optional(),
+  state: z.string().optional(),
+  site_url: z.string().optional(),
+})
+
+const zodValidationPipe = new ZodValidationPipe(createBodySchema)
+
+type CreateBodySchema = z.infer<typeof createBodySchema>
+
+@Controller('/companies')
+export class CreateCompanyController {
+  constructor(private createCompany: CreateCompanyUseCase) {}
+
+  @Post()
+  @Public()
+  async create(@Body(zodValidationPipe) body: CreateBodySchema) {
+    const { userId, cnpj, city, state, site_url } = body
+
+    const result = await this.createCompany.execute({
+      userId,
+      cnpj,
+      city,
+      state,
+      site_url,
+    })
+
+    if (result.isLeft()) {
+      const error = result.value
+
+      if (error instanceof NotAllowedError)
+        switch (error.constructor) {
+          case NotAllowedError:
+            throw new NotAcceptableException()
+          default:
+            throw new BadRequestException(error.message)
+        }
+    }
+  }
+}
