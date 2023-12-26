d46d4dbf13e67c2f90f3b2b177b27de099438a36
Jackson <jackson1712@hotmail.com>
2023-12-26 16:56:35 -0300

    feat: new test e2e to creation a company

---
+++ src/infra/http/controllers/create-company.controller.e2e-spec.ts
@@ -0,0 +1,48 @@
+import request from 'supertest'
+import { AppModule } from '@/infra/app.module'
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { UserFactory } from 'test/factories/make-user'
+
+describe('Create Company Controller(E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+  let userFactory: UserFactory
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [UserFactory],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+    prisma = moduleRef.get(PrismaService)
+    userFactory = moduleRef.get(UserFactory)
+
+    await app.init()
+  })
+
+  test('[POST] /companies', async () => {
+    const user = await userFactory.makePrismaUser()
+
+    const response = await request(app.getHttpServer())
+      .post('/companies')
+      .send({
+        userId: user.id.toString(),
+        cnpj: '12345678000145',
+      })
+
+    expect(response.statusCode).toEqual(201)
+
+    const companyOnDatabase = await prisma.company.findUnique({
+      where: {
+        userId: user.id.toString(),
+      },
+    })
+
+    expect(companyOnDatabase).toBeTruthy()
+    expect(companyOnDatabase?.cnpj).toEqual('12345678000145')
+  })
+})
