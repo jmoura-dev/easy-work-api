56e72e424bbced3356504bbe02dab8874f6d11a6
Jackson <jackson1712@hotmail.com>
2023-12-28 11:56:58 -0300

    feat: create new prisma repository to candidature and job

---
+++ src/infra/database/prisma/repositories/prisma-candidatures-repository.ts
@@ -0,0 +1,59 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
+import { CandidaturesRepository } from '@/domain/easy-work/application/repositories/candidatures-repository'
+import { Candidature } from '@/domain/easy-work/enterprise/entities/candidature'
+import { PrismaCandidatureMapper } from '../mappers/prisma-candidature-mapper'
+import { PrismaService } from '../prisma.service'
+
+export class PrismaCandidaturesRepository implements CandidaturesRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async create(candidature: Candidature): Promise<void> {
+    const data = PrismaCandidatureMapper.toPrisma(candidature)
+
+    await this.prisma.candidature.create({
+      data,
+    })
+  }
+
+  async findById(id: string): Promise<Candidature | null> {
+    const candidature = await this.prisma.candidature.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    if (!candidature) {
+      return null
+    }
+
+    return PrismaCandidatureMapper.toDomain(candidature)
+  }
+
+  async findManyByJobId(
+    { page }: PaginationParams,
+    jobId: string,
+  ): Promise<Candidature[]> {
+    const candidatures = await this.prisma.candidature.findMany({
+      where: {
+        jobId,
+      },
+      skip: (page - 1) * 20,
+      take: 20,
+    })
+
+    return candidatures.map((candidature) =>
+      PrismaCandidatureMapper.toDomain(candidature),
+    )
+  }
+
+  async save(candidature: Candidature): Promise<void> {
+    const data = PrismaCandidatureMapper.toPrisma(candidature)
+
+    await this.prisma.candidature.update({
+      where: {
+        id: data.id,
+      },
+      data,
+    })
+  }
+}
