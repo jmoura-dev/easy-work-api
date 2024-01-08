c23195cf566c50b778a636d3181dc6d0e627d836
Jackson <jackson1712@hotmail.com>
2024-01-08 17:51:33 -0300

    feat: add new test e2e to upload and create avatar

---
+++ src/infra/http/controllers/upload-avatar.controller.e2e-spec.ts
@@ -0,0 +1,43 @@
+import { AppModule } from '@/infra/app.module'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+import { UserFactory } from 'test/factories/make-user'
+
+describe('Upload Avatar Controller (E2E)', () => {
+  let app: INestApplication
+  let jwt: JwtService
+  let userFactory: UserFactory
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [UserFactory],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+    jwt = moduleRef.get(JwtService)
+    userFactory = moduleRef.get(UserFactory)
+
+    await app.init()
+  })
+
+  test('[POST] /avatar', async () => {
+    const user = await userFactory.makePrismaUser()
+    const accessToken = jwt.sign({ sub: user.id.toString() })
+
+    const response = await request(app.getHttpServer())
+      .post('/avatar')
+      .set('Authorization', `Bearer ${accessToken}`)
+      .attach('file', './test/e2e/sample-upload.png')
+
+    console.log(response)
+
+    expect(response.statusCode).toEqual(201)
+    expect(response.body).toEqual({
+      avatarId: expect.any(String),
+    })
+  })
+})
