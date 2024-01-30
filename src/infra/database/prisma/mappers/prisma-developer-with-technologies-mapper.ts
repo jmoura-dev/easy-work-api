7ac175259e22edf83c09eea925ab4d3da4cf6c9e
Jackson <jackson1712@hotmail.com>
2024-01-30 19:07:20 -0300

    feat: add new mapper to developer with technologies

---
+++ src/infra/database/prisma/mappers/prisma-developer-with-technologies-mapper.ts
@@ -0,0 +1,33 @@
+import {
+  Technology as PrismaTechnology,
+  User as PrismaUser,
+  Developer as PrismaDeveloper,
+} from '@prisma/client'
+import { DeveloperWithTechnologies } from '@/domain/easy-work/enterprise/entities/value-objects/developer-with-technologies'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { PrismaTechnologyMapper } from './prisma-technology-mapper'
+
+type PrismaDeveloperWithTechnologies = PrismaDeveloper & {
+  user: PrismaUser
+  developerTechnology: {
+    technology: PrismaTechnology
+  }[]
+}
+
+export class PrismaDeveloperWithTechnologiesMapper {
+  static toDomain(
+    raw: PrismaDeveloperWithTechnologies,
+  ): DeveloperWithTechnologies {
+    return DeveloperWithTechnologies.create({
+      developerId: new UniqueEntityID(raw.id),
+      userName: raw.user.name,
+      occupation_area: raw.occupation_area,
+      price_per_hour: Number(raw.price_per_hour),
+      available_for_contract: raw.available_for_contract ?? false,
+      about: raw.user.about,
+      techs: raw.developerTechnology.map((tech) =>
+        PrismaTechnologyMapper.toDomain(tech.technology),
+      ),
+    })
+  }
+}
