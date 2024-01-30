f69b1a7e94ca58fd641458471368836d39df2939
Jackson <jackson1712@hotmail.com>
2024-01-30 19:06:33 -0300

    feat: add new presenter to developer with technologies

---
+++ src/infra/http/presenters/developer-with-technologies-presenter.ts
@@ -0,0 +1,15 @@
+import { DeveloperWithTechnologies } from '@/domain/easy-work/enterprise/entities/value-objects/developer-with-technologies'
+
+export class DeveloperWithTechnologiesPresenter {
+  static toHTTP(developerWithTechnologies: DeveloperWithTechnologies) {
+    return {
+      developerId: developerWithTechnologies.developerId.toString(),
+      userName: developerWithTechnologies.userName,
+      occupation_area: developerWithTechnologies.occupation_area,
+      price_per_hour: developerWithTechnologies.price_per_hour,
+      available_for_contract: developerWithTechnologies.available_for_contract,
+      about: developerWithTechnologies.about,
+      techs: developerWithTechnologies.techs,
+    }
+  }
+}
